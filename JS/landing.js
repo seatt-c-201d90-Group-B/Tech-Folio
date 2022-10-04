31c2fdb0dc5271c179160840e6b3b12b6ae9c9dd
Elias <elias.staehle@gmail.com>
2022-10-04 15:07:09 -0400

    added preview page logic

--- JS/landing.js
+++ JS/landing.js
@@ -6,7 +6,7 @@ let formBio = document.getElementById('formBio');
 let projectForm = document.getElementById('projectForm');
 let cardList = document.getElementById('cardList');
 let cardsStored = [];
-
+let clearButton = document.getElementById('resetBtn');
 // creating constructor for cards objects
 function Bio(name, aboutMe, position) {
   this.name = name;
@@ -29,18 +29,23 @@ function loadBio() {
 function loadCards() {
   const cardsString = JSON.stringify(cards);
   localStorage.setItem('cards', cardsString);
+
 }
-function addBio() {
+function addBio(event) {
+  event.preventDefault();
   let name = document.getElementById('name').value;
   let aboutMe = document.getElementById('aboutMe').value;
   let position = document.getElementById('position').value;
   new Bio(name, aboutMe, position);
+  if (userInfo.length > 1) {
+    userInfo.shift();
+  }
   loadBio();
- 
+
 }
-function cardsParsed(){
+function cardsParsed() {
   cardsStored = JSON.parse(localStorage.getItem('cards')) || [];
-  
+
 }
 
 
@@ -51,50 +56,60 @@ function addCard(event) {
   let link = document.getElementById('link').value;
   new Cards(title, info, link);
   loadCards();
-  while(ul.firstChild){
+  while (ul.firstChild) {
     ul.removeChild(ul.lastChild);
   }
   cardsParsed();
-  render();
+  renderCards();
   projectForm.reset();
 }
 
 // creating render function
+function renderBio() {
+
+}
 let ul = document.createElement('ul');
 
 cardList.appendChild(ul);
-function render() {
+function renderCards() {
+  cardsParsed();
   for (let i = 0; i < cardsStored.length; i++) {
     let li = document.createElement('li');
     ul.appendChild(li);
     let h3 = document.createElement('h3');
-    h3.innerText = cards[i].title;
+    h3.innerText = cardsStored[i].title;
     li.appendChild(h3);
     let deleteProject = document.createElement('p');
     deleteProject.innerText = 'X';
     deleteProject.id = i;
     li.appendChild(deleteProject);
     let p = document.createElement('p');
-    p.innerText = cards[i].info;
+    p.innerText = cardsStored[i].info;
     li.appendChild(p);
     let a = document.createElement('a');
-    a.href = cards[i].link;
+    a.href = cardsStored[i].link;
     a.innerText = 'Project';
     li.appendChild(a);
   }
 }
 // add remove project function
-function removeProject(event){
-  if(event.target.textContent === 'X'){
+function removeProject(event) {
+  if (event.target.textContent === 'X') {
     cardsStored.splice(event.target.id, 1);
     cards.splice(event.target.id, 1);
-    while(ul.firstChild){
+    while (ul.firstChild) {
       ul.removeChild(ul.lastChild);
     }
     localStorage.setItem('cards', JSON.stringify(cardsStored));
-    render();
+    renderCards();
   }
 }
+function restartPage() {
+  localStorage.clear();
+  location.href = 'index.html';
+}
+cardsParsed();
+renderCards();
 
 
 
@@ -121,7 +136,7 @@ function removeProject(event){
 
 
 
-
+clearButton.addEventListener('click', restartPage);
 ul.addEventListener('click', removeProject);
 formBio.addEventListener('submit', addBio);
 projectForm.addEventListener('submit', addCard);
--- JS/preview.js
+++ JS/preview.js
@@ -1,2 +1,61 @@
+// grabbing local storage
+let bioStored = JSON.parse(localStorage.getItem('userInfo')) || [];
+let cardsStored = JSON.parse(localStorage.getItem('cards')) || [];
 
+let cardsList = document.getElementById('flipCardList');
+let resetButton = document.getElementById('resetBtn');
+let cardDisplay = [];
+function randNum() {
+  return Math.floor(Math.random() * cardsStored.length);
 
+}
+randNum();
+function rendBio() {
+  let name = document.getElementById('aboutMeName');
+  name.textContent = bioStored[0].name;
+  let position = document.getElementById('aboutPosi');
+  position.textContent = bioStored[0].position;
+  let aboutMe = document.getElementById('aboutMeP');
+  aboutMe.textContent = bioStored[0].aboutMe;
+}
+rendBio();
+function displayRandCardNum() {
+  if (cardsStored.length > 6) {
+    while (cardDisplay.length < 6) {
+      let numRand = randNum();
+      if (!cardDisplay.includes(numRand)) {
+        cardDisplay.push(numRand);
+      }
+    }
+  } if (cardsStored.length < 6) {
+    while (cardDisplay.length < cardsStored.length) {
+      let numRand = randNum();
+      if (!cardDisplay.includes(numRand)) {
+        cardDisplay.push(numRand);
+      }
+    }
+  }
+}
+displayRandCardNum();
+function rendCard(){
+  for (let i = 0; i < cardDisplay.length; i++) {
+    let li = document.createElement('li');
+    cardsList.appendChild(li);
+    let h3 = document.createElement('h3');
+    h3.innerText = cardsStored[cardDisplay[i]].title;
+    li.appendChild(h3);
+    let p = document.createElement('p');
+    p.innerText = cardsStored[cardDisplay[i]].info;
+    li.appendChild(p);
+    let a = document.createElement('a');
+    a.href = cardsStored[cardDisplay[i]].link;
+    a.innerText = 'Project';
+    li.appendChild(a);
+  }
+}
+rendCard();
+function restartAll(){
+  localStorage.clear();
+  location.href='index.html';
+}
+resetButton.addEventListener('click', restartAll);
